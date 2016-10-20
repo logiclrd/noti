75489435ecbbf7fa8fbe845d0fad45bb8464d15a
Jaime Piña <jaime@variadi.co>
2016-10-19 22:40:09 -0700

    Add trigger help

--- cli/banner/nsuserhelp_darwin.go
+++ cli/banner/nsuserhelp_darwin.go
@@ -15,6 +15,19 @@ OPTIONS
     -sound
         Notification sound.
 
+    -trigger 'mode[=value]'
+        Set notification trigger. You can set this flag multiple times to set
+        multiple triggers.
+
+        exit
+            Trigger a notification when the process exits. Default.
+        match=<string>
+            Trigger a notification when the running command prints a string to
+            stdout or stderr.
+        timeout=<duration>
+            Trigger a notification and kill the running command after a certain
+            duration.
+
     -v, -verbose
         Enable verbose mode.
     -h, -help
--- cli/root/help.go
+++ cli/root/help.go
@@ -3,6 +3,19 @@ package root
 var helpText = `noti [options] [notification [options]] [command]
 
 OPTIONS
+    -trigger 'mode[=value]'
+        Set notification trigger. You can set this flag multiple times to set
+        multiple triggers.
+
+        exit
+            Trigger a notification when the process exits. Default.
+        match=<string>
+            Trigger a notification when the running command prints a string to
+            stdout or stderr.
+        timeout=<duration>
+            Trigger a notification and kill the running command after a certain
+            duration.
+
     -v, -verbose
         Enable verbose mode.
     -h, -help
