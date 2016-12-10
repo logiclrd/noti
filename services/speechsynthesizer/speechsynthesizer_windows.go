87c022ac60b73005e5c891bab4e74abac323e8eb
Jaime Piña <jaime@variadi.co>
2016-12-10 00:28:46 -0800

    Add speechsynthesizer

--- services/speechsynthesizer/speechsynthesizer_windows.go
+++ services/speechsynthesizer/speechsynthesizer_windows.go
@@ -1,5 +1,23 @@
 package speechsynthesizer
 
+import "bytes"
+import "text/template"
+import "os/exec"
+import "os"
+
+const script = `
+Add-Type -AssemblyName System.speech
+$n = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+$n.Rate = {{.Rate}}
+
+{{with .Voice}}
+$n.SelectVoice("{{.}}")
+{{end}}
+
+$n.Speak("{{.Text}}")
+`
+
 type Notification struct {
 	Text string
 	// Rate is from -10 to 10. -10 is slowest.
@@ -7,5 +25,21 @@ type Notification struct {
 	Voice string
 }
 
-// $n = New-Object System.Speech.Synthesis.SpeechSynthesizer
-// $n.Speak('Hello')
+func (n *Notification) Send() error {
+	tmpl, err := template.New("").Parse(script)
+	if err != nil {
+		return err
+	}
+
+	buf := new(bytes.Buffer)
+	if err := tmpl.Execute(buf, n); err != nil {
+		return err
+	}
+
+	cmd := exec.Command("PowerShell", "-Command", buf.String())
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
+
