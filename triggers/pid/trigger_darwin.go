a4182e92c362f255122f071b82749d7517b7555e
Jaime Piña <jaime@variadi.co>
2017-02-13 16:25:48 -0800

    Add macOS pid trigger

--- triggers/pid/trigger_darwin.go
+++ triggers/pid/trigger_darwin.go
@@ -1,7 +1,37 @@
 package pid
 
-import "github.com/variadico/noti/runstat"
+import (
+	"os"
+	"syscall"
+	"time"
+
+	"github.com/variadico/noti/runstat"
+)
 
 func (t *Trigger) Run(cmdErr chan error, stats chan runstat.Result) {
-	// Not implemented.
+	proc, err := os.FindProcess(t.pid)
+	if err != nil {
+		t.stats.Err = err
+		stats <- t.stats
+		return
+	}
+
+	for {
+		select {
+		case <-t.ctx.Done():
+			return
+		default:
+			// Check if PID exists.
+
+			// If sending sig 0 works, then process is still alive.
+			err = proc.Signal(syscall.Signal(0))
+			if err != nil {
+				t.stats.Err = err
+				stats <- t.stats
+				return
+			}
+
+			time.Sleep(2 * time.Second)
+		}
+	}
 }
--- triggers/pid/trigger_windows.go
+++ triggers/pid/trigger_windows.go
@@ -1,7 +1,12 @@
 package pid
 
-import "github.com/variadico/noti/runstat"
+import (
+	"log"
+
+	"github.com/variadico/noti/runstat"
+)
 
 func (t *Trigger) Run(cmdErr chan error, stats chan runstat.Result) {
 	// Not implemented.
+	log.Println("PID TRIGGER NOT IMPLEMENTED")
 }
